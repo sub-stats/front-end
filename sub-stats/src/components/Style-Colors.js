7a27589d844d923911eb7cd3e8cb5f5fedf34043
Faye Skeen <fskeen@gmail.com>
2019-07-29 15:03:00 -0700

    Added colors variable page for styling.

---
+++ sub-stats/src/components/Style-Colors.js
@@ -0,0 +1,30 @@
+/*
+This page contains color variables for consistent styling. Colors are stored as strings in an object; access the colors using dot notation:
+
+(example using styled components)
+
+const HeaderDiv = styled.div`
+        background-color: ${colors.primary.mint};
+    `
+
+*/
+
+export const colors = {
+    orangered: {
+        orangered: "#FF4500",
+        mint: "#0DD3BB",
+        blue: "#24A0ED",
+    },
+    secondary: {
+        yellow: "#FFCA00",
+        mango: "#FFB000",
+        orange: "#FF8717",
+        teal: "#00A6A5",
+        alienblue: "#0079D3"
+    },
+    neutral: {
+        white: "#fff",
+        black: "#000",
+        redditgrey: "#DAE0E6"
+    }
+  };
