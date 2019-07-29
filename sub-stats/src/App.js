8fb21015c4f3b5960f6b74314be3fc23d5e095ea
TheIllusiveC4 <gasingdong@gmail.com>
2019-07-29 14:34:34 -0700

    add basic tabs for summary and comparison

--- sub-stats/src/App.js
+++ sub-stats/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import NavTabs from './components/NavTabs';
 
 function App() {
   return (
     <div className="App">
-
+      <NavTabs></NavTabs>
     </div>
   );
 }
