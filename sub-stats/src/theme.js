eabadf68e0b15de6c6c080ab366f249f2b507b6a
TheIllusiveC4 <gasingdong@gmail.com>
2019-07-30 11:36:14 -0700

    tweak contrast text in theme

--- sub-stats/src/theme.js
+++ sub-stats/src/theme.js
@@ -6,10 +6,12 @@ import { Colors } from "./components/Style-Colors";
 const theme = createMuiTheme({
   palette: {
     primary: {
-      main: `${Colors.primary.blue}`
+      main: `${Colors.primary.blue}`,
+      contrastText: `${Colors.neutral.white}`
     },
     secondary: {
-      main: `${Colors.primary.orangered}`
+      main: `${Colors.primary.orangered}`,
+      contrastText: `${Colors.neutral.white}`
     },
     error: {
       main: `${Colors.secondary.yellow}`
