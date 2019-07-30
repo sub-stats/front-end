18b76eaf33fc2e33a04d28ee6bc4ba143127ee1c
onetrckchris <onetrckchris@gmail.com>
2019-07-30 11:50:07 -0700

    styled form inputs. Need to style Log In button

--- sub-stats/src/views/onboarding-view/components/Login.js
+++ sub-stats/src/views/onboarding-view/components/Login.js
@@ -1,14 +1,53 @@
 import React from 'react';
 import { Form, Field, withFormik } from 'formik';
 import * as Yup from 'yup';
+import styled from 'styled-components';
 
-const Login = () => {
+const RedditForm = styled(Form)`
+    display: flex;
+    flex-direction: column;
+    width: 250px;
+    margin: auto;
+`;
+
+const RedditField = styled(Field)`
+    font-size: 14px;
+    border-radius: 5px;
+    outline: none;
+    background-color: rgb(246, 247, 248);
+    border: 1px solid rgb(237, 239, 241);
+    margin-bottom: 15px;
+    width: 100%;
+    padding: 8px 16px;
+
+    &::placeholder {
+        font-weight: 100;
+    }
+
+    &:hover, &:focus {
+        border-color: rgb(0, 121, 211);
+        background-color: white;
+    }
+`;
+
+const Logo = styled.img`
+    height: 60px;
+    margin-bottom: 10px;
+    margin-top: 50px;
+`;
+
+const Login = ({errors, touched}) => {
     return (
-        <Form>
-            <Field type="text" placeholder="Username" name="username" autoComplete="off" />
-            <Field type="email" placeholder="Email" name="email" autoComplete="off" />
-            <button type="submit">Log In</button>
-        </Form>
+        <>
+            <Logo src="./imgs/reddit-logo.png" alt="#" />
+            <RedditForm>
+                <RedditField type="text" placeholder="Username" name="username" autoComplete="off" />
+                {/* <p>{touched.username && errors.username}</p> */}
+                <RedditField type="email" placeholder="Email" name="email" autoComplete="off" />
+                {/* <p>{touched.email && errors.email}</p> */}
+                <button type="submit">Log In</button>
+            </RedditForm>
+        </>
     )
 }
 
