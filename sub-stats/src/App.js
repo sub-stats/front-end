0a13498414f7f44d0f7b5885239db5df9da2b4ce
Faye Skeen <fskeen@gmail.com>
2019-07-29 17:55:40 -0700

    Added structural div wrapper to App.js

--- sub-stats/src/App.js
+++ sub-stats/src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+
+import styled from 'styled-components';
+
 import NavTabs from './components/NavTabs';
+import MainContentContainer from './components/MainContent/MainContentContainer';
+
 
 function App() {
   return (
     <div className="App">
-      <NavTabs></NavTabs>
+    {/* Sidebar can go here */}
+      <div>
+        <NavTabs></NavTabs>
+        <MainContentContainer />
+      </div>
     </div>
   );
 }
