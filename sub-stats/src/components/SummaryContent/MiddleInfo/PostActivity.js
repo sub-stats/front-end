04608da68d3b99061dc4bcb7fb47557226c6c829
Faye Skeen <fskeen@gmail.com>
2019-07-31 00:46:00 -0700

    Displaying temporary (?) graph in iframe.

--- sub-stats/src/components/SummaryContent/MiddleInfo/PostActivity.js
+++ sub-stats/src/components/SummaryContent/MiddleInfo/PostActivity.js
@@ -2,17 +2,21 @@ import React from 'react';
 import { Card } from '@material-ui/core';
 import styled from 'styled-components';
 
-import { Colors } from '../../Style-Colors'
+import { Colors, PrimaryCard } from '../../Style-Colors'
 
-const StyledTitle = styled.h5`
-    color: ${Colors.secondary.alienblue};
+const StyledIframe = styled.iframe`
+  height: 39rem;
+  width: 100%;
+  border: none;
+  border-radius: 1rem;
 `;
 
 function PostActivity () {
     return (
-        <Card>
-            <StyledTitle>I AM A POST ACTIVITY GRAPH SHOWING 24 HOURS OF AVERAGE POSTING AND COMMENTING ACTIVITY</StyledTitle>
-        </Card>
+        
+        <PrimaryCard>
+            <StyledIframe src="https://sub-stats-dash.herokuapp.com/"></StyledIframe>
+        </PrimaryCard>
     )
 }
 
