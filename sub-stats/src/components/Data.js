b96b30a23ecea652adf894e09e41679b149950a3
TheIllusiveC4 <gasingdong@gmail.com>
2019-08-01 13:57:32 -0700

    add data for subreddit descriptions

--- sub-stats/src/components/Data.js
+++ sub-stats/src/components/Data.js
@@ -17,7 +17,7 @@ export const subArray = [{
         "water energy air temperature light"]
 }, {
     name: "AskHistorians",
-    description: "r/AskHistorians is a subreddit ",
+    description: "r/AskHistorians is a subreddit that aims to provide serious, academic-level answers to questions about history. Questions should be about an event or person or culture in history, or should be about historical methods or techniques.",
     topics: ["history time women people period",
         "war time thanks people sources",
         "people time century word fact",
@@ -25,7 +25,7 @@ export const subArray = [{
         "time depth sort interest rules"]
 }, {
     name: "AskComputerScience",
-    description: "AskComputerScience Voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..",
+    description: "r/AskComputerScience is a subreddit for asking questions about computer science concepts and ideas. Questions about other computer-related topics such as debugging, tech careers, and tech suggestions are not applicable. ",
     topics: ["hash input function people functions",
         "code bytes memory files problems",
         "bit code function time number",
@@ -33,7 +33,7 @@ export const subArray = [{
         "data time problem solution system"]
 }, {
     name: "AskCulinary",
-    description: "AskCulinary Voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..",
+    description: "r/AskCulinary is a subreddit that aims to provide expert guidance for specific cooking problems to help people of all skill levels become better cooks, to increase understanding of cooking, and to share valuable culinary knowledge. ",
     topics: ["bread recipe cooking baking recipes",
         "salt cream rice iron pepper",
         "time chicken chocolate recipe oven",
@@ -41,7 +41,7 @@ export const subArray = [{
         "water heat pan thanks egg"]
 }, {
     name: "TrueAskReddit",
-    description: "TrueAskReddit Voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..",
+    description: "r/TrueAskReddit is a subreddit for intelligent discussion about interesting issues. The subreddit aims to steer clear of low-effort questions such as lists, polls, or those that are easily searchable with defined answers.",
     topics: ["fear reason child food time",
         "games people time today game",
         "people person country traits world",
@@ -49,7 +49,7 @@ export const subArray = [{
         "quality action discussion topic moderators"]
 }, {
     name: "AskSocialScience",
-    description: "AskSocialScience Voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..",
+    description: "r/AskSocialScience is a subreddit that aims to provide great answers to social science questions, based on solid theory, practice, and research. Questions should be novel, specific, and answerable with definitive findings and research.",
     topics: ["criminology sociology differences men women",
         "people information society psychology person",
         "population groups scientists humans concept",
@@ -57,7 +57,7 @@ export const subArray = [{
         "children study time people research"]
 }, {
     name: "AskEngineers",
-    description: "AskEngineers Voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..",
+    description: "r/AskEngineers is a serious discussion-based subreddit with a focus on evidence and logic to answer questions about engineering topics. Questions can be related to engineering careers, current events related to engineering, and even specific computations or formulas.",
     topics: ["job work company time experience",
         "system power heat air pressure",
         "job software position day cars",
@@ -65,7 +65,7 @@ export const subArray = [{
         "people years work job engineers"]
 }, {
     name: "askphilosophy",
-    description: "askphilosophy Voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum..",
+    description: "r/askphilosophy aims to provide serious, well-researched answers to philosophical questions. This subreddit is envisioned as the philosophical counterpart to /r/AskHistorians, which is well-known for its high quality answers to historical questions.",
     topics: ["people p example problem conclusion",
         "time sense argument view self",
         "life time existence point experience",
