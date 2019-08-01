83d572845ae6499beefea8b00aff3b9617a36375
Faye Skeen <fskeen@gmail.com>
2019-07-31 19:22:52 -0700

    Created title card.

--- sub-stats/src/components/SummaryContent/SummaryContentContainer.js
+++ sub-stats/src/components/SummaryContent/SummaryContentContainer.js
@@ -32,9 +32,6 @@ const fake = [{
 }, {
     name: "askphilosophy",
     description: "askphilosophy Voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.."
-}, {
-    name: "explainlikeimfive",
-    description: "explainlikeimfive Voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.."
 }
 ];
 
