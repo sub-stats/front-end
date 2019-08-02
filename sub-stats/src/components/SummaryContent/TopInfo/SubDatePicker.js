3f01e6dc8287bfd61e6ba805c0e286a813694372
TheIllusiveC4 <gasingdong@gmail.com>
2019-08-01 20:12:38 -0700

    disable user from choosing a date range that is too small

--- sub-stats/src/components/SummaryContent/TopInfo/SubDatePicker.js
+++ sub-stats/src/components/SummaryContent/TopInfo/SubDatePicker.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField } from '@material-ui/core';
 // import { DatePicker, MuiPickersUtilsProvider } from '@material-ui/pickers';
 // import DateFnsUtils from '@date-io/date-fns';
@@ -8,6 +8,11 @@ import { DatePicker } from 'antd';
 const { RangePicker } = DatePicker;
 
 export default function SubDatePicker({ label, setStartDate, setEndDate }) {
+  const [currentStart, setCurrentStart] = useState(moment('2019-05-01'));
+
+  const handleCalendarChange = (date, dateString) => {
+    setCurrentStart(date[0]);
+  }
 
   const handleChange = (date, dateString) => {
     setStartDate(dateString[0]);
@@ -15,7 +20,8 @@ export default function SubDatePicker({ label, setStartDate, setEndDate }) {
   }
 
   const disabledDate = (current) => {
-    return current && (current < new Date('2019-05-01') || current > new Date('2019-08-01'));
+    const daysToStart = Math.abs(moment(current).diff(currentStart, 'days'));
+    return current && (current < new Date('2019-05-01') || current > new Date('2019-08-01') || daysToStart <= 3);
   }
 
   return (
@@ -23,7 +29,8 @@ export default function SubDatePicker({ label, setStartDate, setEndDate }) {
       <RangePicker 
         label={label}
         defaultValue={[moment('2019-05-01'), moment('2019-07-31')]}
-        disabledDate={disabledDate} 
+        disabledDate={disabledDate}
+        onCalendarChange={handleCalendarChange}
         onChange={handleChange}
       />
     </form>
